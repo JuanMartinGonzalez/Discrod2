3046e1ac19ea9b3b77720c231717ac1508d6173a
Juan <juanmanuelyarza@hotmail.com>
2025-07-07 20:32:58 -0300

    Se agrego la clase mensajes, correcion gral.

---
+++ Proyecto Discrod 2/BE/Mensajes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Discrod_2.BE
+{
+    public class Mensajes
+    {
+        public string Texto { get; set; }
+        public DateTime FechaEnvio { get; set; }
+        public DateTime FechaLectura { get; set; }
+        public Usuarios UsuarioOrigen { get; set; }
+        public Usuarios UsuarioDestino { get; set; }
+
+        public Mensajes(string texto, DateTime fechaEnvio, DateTime fechaLectura, Usuarios usuarioOrigen, Usuarios usuarioDestino)
+        {
+            Texto = texto;
+            FechaEnvio = fechaEnvio;
+            FechaLectura = fechaLectura;
+            UsuarioOrigen = usuarioOrigen;
+            UsuarioDestino = usuarioDestino;
+        }
+    }
+}
